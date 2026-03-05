7fb3ee87814e6ee66a7773a17d9fbe5f665c655f
Kitti0403 <annus.kitti@gmail.com>
2026-03-05 11:11:59 +0100

    konzolos c#

---
+++ OraiGyak/Konzolos1/Expeditioners.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas_1
+{
+    internal class Expeditioners
+    {
+        // Expeditioners.txt
+        // Id;Nev;Fegyver;Eletpont;Tamadas;Vedelem;Elem;Eletkor;ExpedicioTag
+        // 1;Gustave;Mernoki Penge;1200;150;200;Fold;35;true
+
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Fegyver { get; set; }
+        public int Eletpont { get; set; }
+        public int Tamadas { get; set; }
+
+        public int Vedelem { get; set; }
+        public string Elem { get; set; }
+        public int Eletkor { get; set; }
+        public bool ExpedicioTag { get; set; }
+
+        // Fájl beolvasása és objektumok létrehozása, első sort kihagyva
+
+        public Expeditioners(string sor)
+        {
+            // split ;
+            string[] s = sor.Split(';');
+            try
+            {
+                Id = int.Parse(s[0]);
+                Nev = s[1];
+                Fegyver = s[2];
+                Eletpont = int.Parse(s[3]);
+                Tamadas = int.Parse(s[4]);
+                Vedelem = int.Parse(s[5]);
+                Elem = s[6];
+                Eletkor = int.Parse(s[7]);
+                ExpedicioTag = bool.Parse(s[8]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba beolvasáskor: " + ex.Message);
+            }
+        }
+
+        public static List<Expeditioners> LoadFromTXT(string fajlnev)
+        {
+            List<Expeditioners> karakterek = new();
+
+            try
+            {
+                string[] sorok = File.ReadAllLines(fajlnev);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    karakterek.Add(new Expeditioners(sorok[i]));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Olvasási hiba!");
+            }
+
+            return karakterek;
+
+        }
+    }
+}
---
+++ OraiGyak/Konzolos1/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Reflection.Metadata;
+using System;
+
+namespace Gyakorlas_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Expeditioners> karakterek = Expeditioners.LoadFromTXT("expeditioners.txt");
+
+            // 1. Írasd ki azokat a karaktereket, akiknek a neve 5 karakternél hosszabb!
+            Console.WriteLine("5 karakternél hosszabb nevű karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Nev.Length > 5)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 2. Írasd ki azokat a karaktereket, akiknek a fegyvere "Penge" vagy "Kard"!
+            Console.WriteLine("\nPenge vagy Kard fegyverű karakterek:");
+            Console.WriteLine(
+                string.Join("\n", karakterek.Where(k => k.Fegyver.Contains("Penge") || k.Fegyver.Contains("Kard")).Select(k => k.Nev))
+            );
+
+            //  3. Írasd ki azokat a karaktereket, akiknek az élete 1000-nél több!
+            Console.WriteLine("\n1000-nél több életű karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Eletpont > 1000)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 4. Írasd ki azokat a karaktereket, akiknek a támadása 150-nél több!
+
+            Console.WriteLine("\n150-nél több támadású karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Tamadas > 150)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 5.Írasd ki azokat a karaktereket, akiknek a védelme 150 - nél több!
+
+            Console.WriteLine("\n150-nél több védelmi értékű karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Vedelem > 150)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 6.Írasd ki azokat a karaktereket, akiknek az élete 1000 - nél több és a támadása 150 - nél több!
+
+            Console.WriteLine("\n1000-nél több életű és 150-nél több támadású karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Eletpont > 1000 && k.Tamadas > 150)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 7.Írasd ki azokat a karaktereket, akiknek az élete 1000 - nél több és a védelme 150 - nél több!
+
+            Console.WriteLine(
+                "\n1000-nél több életű és 150-nél több védelmi értékű karakterek:"
+            );
+            foreach (var k in karakterek)
+            {
+                if (k.Eletpont > 1000 && k.Vedelem > 150)
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+
+            // 8.Metódus segítségével kérd be a felhasználótól a karakterek adatait és add hozzá a listához!
+
+            Console.WriteLine(
+                "\nÚj karakter hozzáadása:"
+            );
+
+            Expeditioners ujKarakter = new Expeditioners("0; ; ;0;0;0; ;0;false");
+            // + Hibakezelés
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Név: ");
+                    ujKarakter.Nev = Console.ReadLine();
+
+                    Console.Write("Fegyver: ");
+                    ujKarakter.Fegyver = Console.ReadLine();
+
+                    Console.Write("Életpont: ");
+                    ujKarakter.Eletpont = int.Parse(Console.ReadLine());
+
+                    Console.Write("Támadás: ");
+                    ujKarakter.Tamadas = int.Parse(Console.ReadLine());
+
+                    Console.Write("Védelem: ");
+                    ujKarakter.Vedelem = int.Parse(Console.ReadLine());
+
+                    Console.Write("Elem: ");
+                    ujKarakter.Elem = Console.ReadLine();
+
+                    Console.Write("Életkor: ");
+                    ujKarakter.Eletkor = int.Parse(Console.ReadLine());
+
+                    Console.Write("Expedíció tag (true/false): ");
+                    ujKarakter.ExpedicioTag = bool.Parse(Console.ReadLine());
+
+                    break; // Sikeres adatbevitel, kilépünk a ciklusból
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Hibás adat, próbáld újra! " + ex.Message);
+                }
+            }
+
+            karakterek.Add(ujKarakter);
+
+            if (karakterek.Contains(ujKarakter))
+            {
+                Console.WriteLine("\nÚj karakter hozzáadva: " + ujKarakter.Nev);
+            }
+            else
+            {
+                Console.WriteLine("\nHiba történt az új karakter hozzáadásakor.");
+            }
+
+            // 9.Átlag életkor
+
+            double atlagEletkor = karakterek.Average(k => k.Eletkor);
+            Console.WriteLine("\nÁtlag életkor: " + atlagEletkor);
+
+            // 10.Kik azok akiknek "Tuz" az eleme!
+
+            Console.WriteLine("\n'Tuz' elemű karakterek:");
+            foreach (var k in karakterek)
+            {
+                if (k.Elem == "Tuz")
+                {
+                    Console.WriteLine(k.Nev);
+                }
+            }
+        }
+    }
+}
